6d468d4c7151ca68d1aa30dfb468ff34fcd2715e
Tesserex <tesserex@gmail.com>
2025-10-22 20:23:12 -0500

    Remove a bunch of blower-san stuff

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import * as Phaser from 'phaser';
-import GameScene from './scenes/MainMenu';
-import MainScene from './scenes/MainScene';
-import GameOver from './scenes/GameOver';
-import TutorialScene from './scenes/TutorialScene';
-import GameWon from './scenes/GameWon';
 import MessageBus from './messageBus/MessageBus';
 import getRealStorageProvider from './messageBus/realStorageProvider';
 
@@ -13,14 +8,14 @@ new Phaser.Game({
 	type: Phaser.AUTO,
 	parent: 'game',
 	backgroundColor: '#aaaaee',
-	title: 'Something About Wind!',
+	title: 'Minijam Template',
 	scale: {
 		width: 1920,
 		height: 1080,
 		mode: Phaser.Scale.FIT,
 		autoCenter: Phaser.Scale.CENTER_BOTH
 	},
-	scene: [GameScene, MainScene, GameOver, TutorialScene, GameWon],
+	scene: [],
 	physics: {
 		default: 'arcade',
 		arcade: {
--- src/messageBus/Messages.ts
+++ src/messageBus/Messages.ts
@@ -1,8 +1,1 @@
-export const Messages = {
-	SecondElapsed: 'SecondElapsed',
-	GameOver: 'GameOver',
-	BallScale: 'BallScale',
-	DialogueComplete: 'DialogueComplete',
-	PlayerScore: 'PlayerScore',
-	HighScore: 'HighScore'
-};
+export const Messages = {};
